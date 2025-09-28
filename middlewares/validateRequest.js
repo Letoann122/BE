e9aa68f69caa52fd399509358868032e67a6b419
Le Toan <lvt1222003@gmail.com>
2025-09-28 12:33:44 +0700

    update feat/register_client

--- middlewares/validateRequest.js
+++ middlewares/validateRequest.js
@@ -3,7 +3,6 @@ const { validationResult } = require("express-validator");
 const validateRequest = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    // Gom các message lại thành mảng
     const errorMessages = errors.array().map(err => err.msg);
     return res.status(422).json({
       status: false,
