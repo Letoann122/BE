adc0948c3e46e0ebe75b2ccdfad807ec094b8827
chipewado <phamtantaihocsinh@gmail.com>
2025-09-30 23:52:53 +0700

    chưa xong còn reset

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,7 +4,7 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const { validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
-
+const nodemailer = require("nodemailer");
 dotenv.config();
 
 const UserController = {
@@ -146,6 +146,31 @@ const UserController = {
       error: error.message,
     });
   }
+},
+
+async forgotPassword(req, res) {
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: "maddison53@ethereal.email",
+    pass: "jn7jnAPss4f63QBp6D",
+  },
+});
+
+// Wrap in an async IIFE so we can use await.
+(async () => {
+  const info = await transporter.sendMail({
+    from: '"Maddison Foo Koch" <maddison53@ethereal.email>',
+    to: "bar@example.com, baz@example.com",
+    subject: "Hello ✔",
+    text: "Hello world?", // plain‑text body
+    html: "<b>Hello world?</b>", // HTML body
+  });
+
+  console.log("Message sent:", info.messageId);
+})();
 }
 
 };
